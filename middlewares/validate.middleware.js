858049c92ad4e477410f007f71af82e5cfb5c8bc
Yusuf Abdulrasheed <yusufabdulrasheed200@gmail.com>
2026-02-28 03:14:43 +0100

    implemented DTO validation check using JOI

---
+++ middlewares/validate.middleware.js
@@ -0,0 +1,14 @@
+const validate = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false })
+
+    if(error){
+        return res.status(400).json({
+            success: false,
+            errors: error.details.map((err) => err.message),
+        })
+    }
+
+    next()
+}
+
+export default validate
